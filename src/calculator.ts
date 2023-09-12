07d5e3e8ac636384bfaff95058f3890f1d2ea4b1
Jake Medeiros <jakejmedeiros@gmail.com>
2023-09-12 15:14:09 -0400

    merge

--- src/calculator.ts
+++ src/calculator.ts
@@ -3,13 +3,12 @@ export class Calculator {
     return x + y;
   }
 
-<<<<<<< HEAD
   public subtract(x: number, y: number) : number {
     return x - y;
-=======
+  }
+  
   public divide(x: number, y: number) : number {
     return x / y;
->>>>>>> 0714f06 (implement divide operation)
   }
 
   public multiply(x: number, y: number) : number {
