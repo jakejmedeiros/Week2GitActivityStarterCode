8b0dd93d12a559ce52e2a9edca9dcfc063233e09
Hunter Groff <huntergroff22@gmail.com>
2023-09-12 14:53:57 -0400

    add subtract operation

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,10 @@ import { Calculator } from './calculator';
 
 const calc = new Calculator();
 
-const result_add = calc.add(2, 3);
+const result_add = calc.add(2, 3); //5
 console.log(`result_add = ${result_add}`);
+
+const result_subtract = calc.subtract(6, 2); //4
+console.log(`result_subtract = ${result_subtract}`);
+
 
