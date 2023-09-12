bb7652ce19f783868a2aa4480f1e65b341e7a9e4
Hunter Groff <huntergroff22@gmail.com>
2023-09-12 15:05:22 -0400

    implement multiply operation

--- src/index.ts
+++ src/index.ts
@@ -8,4 +8,6 @@ console.log(`result_add = ${result_add}`);
 const result_subtract = calc.subtract(6, 2); //4
 console.log(`result_subtract = ${result_subtract}`);
 
+const result_multiply = calc.multiply(2, 5); //10
+console.log(`result_multiply= ${result_multiply}`);
 
